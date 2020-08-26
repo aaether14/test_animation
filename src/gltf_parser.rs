ad5b17da5b0c3f0c561a2ae51c82f1a3a88c8145
Stefan Dinescu <aaetherengine@gmail.com>
2020-08-26 22:38:38 +0300

    * cleanup

--- src/gltf_parser.rs
+++ src/gltf_parser.rs
@@ -3,26 +3,20 @@ use anyhow::Context;
 use serde::Deserialize;
 use serde_repr::Deserialize_repr;
 
-#[derive(Debug)]
-struct Buffer(Vec<u8>);
+#[derive(Debug, Deserialize)]
+struct Buffer {
+    #[serde(rename(deserialize = "byteLength"))]
+    byte_length: usize,
+    uri: String
+}
 
 impl Buffer {
-    fn from_json_value<P: AsRef<Path>>(json: serde_json::Value, path: P) -> anyhow::Result<Self> {
-        #[derive(Deserialize)]
-        struct BufferJSON {
-            #[serde(rename(deserialize = "byteLength"))]
-            byte_length: usize,
-            uri: String
-        }
-        let BufferJSON {
-            byte_length,
-            uri
-        } = serde_json::from_value(json)?;
-        let data = std::fs::read(path.as_ref().join(uri))?;
-        if data.len() != byte_length {
+    fn get_data(&self) -> anyhow::Result<Vec<u8>> {
+        let data = std::fs::read(&self.uri)?;
+        if data.len() != self.byte_length {
             return Err(anyhow::anyhow!("The byte length read form the json and the actual one do not match."))
         }
-        Ok(Buffer(data))
+        Ok(data)
     }
 }
 
@@ -73,9 +67,10 @@ struct Accessor {
     count: usize
 }
 
-#[derive(Debug)]
+#[derive(Debug, Deserialize)]
 pub struct ParsedGLTF {
     buffers: Vec<Buffer>,
+    #[serde(rename(deserialize = "bufferViews"))]
     buffer_views: Vec<BufferView>,
     accessors: Vec<Accessor>
 }
@@ -85,27 +80,11 @@ pub fn parse_gltf<P: AsRef<Path>>(path: P) -> anyhow::Result<ParsedGLTF> {
     let canonic_path = std::fs::canonicalize(path.as_ref())?;
     // needed to find associated binary files whose paths are relative to this file
     let directory = canonic_path.parent().context("Could not compute the parent path.")?;
-    let file_contents = std::fs::read_to_string(path.as_ref())?;
-    let json: serde_json::Value = serde_json::from_str(&file_contents)?;
-    let buffers = json.
-        get("buffers").context("Missing 'buffers' field.")?.
-        as_array().context("'buffers' is not array.")?.iter().
-        map(|v| Buffer::from_json_value(v.clone(), &directory)).
-        collect::<anyhow::Result<_>>()?;
-    let buffer_views = json.
-        get("bufferViews").context("Missing 'bufferViews' field.")?.
-        as_array().context("'bufferViews' is not array.")?.iter().
-        map(|v| serde_json::from_value(v.clone()).map_err(From::from)).
-        collect::<anyhow::Result<_>>()?;
-    let accessors = json.
-        get("accessors").context("Missing 'accessors' field.")?.
-        as_array().context("'accessors' is not array.")?.iter().
-        map(|v| serde_json::from_value(v.clone()).map_err(From::from)).
-        collect::<anyhow::Result<_>>()?;
-        
-    Ok(ParsedGLTF {
-        buffers,
-        buffer_views,
-        accessors
-    })
+    let file = std::fs::File::open(path)?;
+    let mut result: ParsedGLTF = serde_json::from_reader(file)?;
+    for b in &mut result.buffers {
+        // update uri to contain the absolute path to the resource
+        b.uri = directory.join(&b.uri).to_str().context("Could not set uri.")?.to_string();
+    }
+    Ok(result)
 }
